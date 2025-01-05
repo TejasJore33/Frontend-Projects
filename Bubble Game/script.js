7b33b9e159cfe4f2f93ac25fbf6017bcd4b6a409
TejasJore33 <tejasjore33@Gmail.com>
2025-01-06 00:24:12 +0530

    .

--- Bubble Game/script.js
+++ Bubble Game/script.js
@@ -1,5 +1,35 @@
-var clutter="";
+var timer =60;
 
-for(var i=1; i<=70; i++){
-   clutter += `<div class="bubble">5</div>`;
+function getNewHit(){
+    var rn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent=rn;
 }
+
+
+function makeBubble() {
+    var clutter="";
+
+for(var i=1; i<=168; i++){
+   const rn =  Math.floor(Math.random()*10);
+   clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML=clutter;
+}
+
+
+
+function runTimer() {
+    var timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timervalue").textContent=timer;
+        } else{
+            clearInterval(timerint);
+        }
+    },1000);
+}
+
+runTimer();
+makeBubble();
+getNewHit();
