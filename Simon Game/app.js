2e27a92adcbb8e9687900283b31ba9343a92b65f
Tejas Jore <116269768+TejasJore33@users.noreply.github.com>
2025-01-20 23:01:34 +0530

    .

---
+++ Simon Game/app.js
@@ -0,0 +1,39 @@
+let gameSeq =[];
+let userSeq=[];
+
+let started = false;
+let level = 0;
+
+let btns =["yellow","red","purple","green"];
+
+let h2= document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("Game is started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function btnFash(btn){
+    btn.classlist.add("flash");
+    setTimeout(function() {
+        btn.classlist.remove("flash");
+    }, 1000);
+}
+
+
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+    let randIdx = Math.floor(Math.random()*3);
+    let randoColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randoColor}`);
+    console.log(randIdx);
+    console.log(randbtn);
+    console.log(randoColor);
+    btnFlash(randbtn);
+
+}
